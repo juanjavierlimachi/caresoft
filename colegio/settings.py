2822d1c478634f2eda449d4e046c4fb86e292e72
juan <sistemasuatf12345@gmail.com>
2017-07-03 19:29:25 -0700

    version0.2

--- colegio/settings.py
+++ colegio/settings.py
@@ -90,12 +90,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
 ]
 
 
